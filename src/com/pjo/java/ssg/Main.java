6855acc70aba8643be058b4bd8da691c8946c749
wjddh12345 <pk4win7@naver.com>
2023-10-05 21:22:53 +0900

    게시물 리스팅 시 숫자 데이터의 출력 자리 수 지정, 조회수 기능 추가

--- src/com/pjo/java/ssg/Main.java
+++ src/com/pjo/java/ssg/Main.java
@@ -8,6 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 == ");
 
+		makeTestArticle();
+		
 		Scanner sc = new Scanner(System.in);
 
 		int lastArticleId = 0;
@@ -47,11 +49,11 @@ public class Main {
 					continue;
 				}
 
-				System.out.println("번호, 제목");
+				System.out.println("번호 | 조회 | 제목");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 
-					System.out.printf("%d, %s\n", article.id, article.title);
+					System.out.printf("%4d | %4d |  %s\n", article.id, article.hit, article.title);
 				}
 			} else if (command.startsWith("article detail ")) {
 				String[] commandBits = command.split(" ");
@@ -72,12 +74,15 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
+				
+				foundArticle.increaseHit();
 
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", "2020-12-12 12:12:12");
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
-
+				System.out.printf("내용 : %s\n", foundArticle.hit);
+				
 			} else if (command.startsWith("article modify ")) {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
@@ -139,6 +144,11 @@ public class Main {
 		sc.close();
 		System.out.println("== 프로그램 끝 == ");
 	}
+
+	private static void makeTestArticle() {
+		// 추후 구현 예정
+		
+	}
 }
 
 class Article {
@@ -146,11 +156,17 @@ class Article {
 	String regDate;
 	String title;
 	String body;
+	int hit;
 
 	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
 		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
+		this.hit = 0;  // 아무도 안본상태
+	}
+	
+	public void increaseHit() {
+		hit++;
 	}
 }
