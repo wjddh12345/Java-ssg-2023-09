515eba997d493220c0c26e93fe0af0b34ad34baa
wjddh12345 <pk4win7@naver.com>
2023-10-05 20:47:07 +0900

    게시물 상세페이지 기능 구현

--- src/com/pjo/java/ssg/Main.java
+++ src/com/pjo/java/ssg/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int lastArticleId = 0;
-		
+
 		List<Article> articles = new ArrayList();
 
 		while (true) {
@@ -28,7 +28,6 @@ public class Main {
 				break;
 			} else if (command.equals("article write")) {
 				int id = lastArticleId + 1;
-				lastArticleId = id;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
@@ -36,28 +35,53 @@ public class Main {
 
 				Article article = new Article(id, title, body);
 				articles.add(article);
-				
+
+				lastArticleId = id;
+
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 			} else if (command.equals("article list")) {
-				if ( articles.size() == 0) {
+				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
 				}
-				
+
 				System.out.println("번호, 제목");
-				for ( int i = articles.size() - 1; i >= 0; i--) {
+				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					
+
 					System.out.printf("%d, %s\n", article.id, article.title);
 				}
+			} else if (command.startsWith("article detail ")) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
 				
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", "2020-12-12 12:12:12");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+
 			} else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
 			}
 		}
 
 		sc.close();
-
 		System.out.println("== 프로그램 끝 == ");
 	}
 }
