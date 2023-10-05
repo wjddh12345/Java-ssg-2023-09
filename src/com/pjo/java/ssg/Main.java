fab5408b74753ba7a252e93d37ce70091c68ff4b
wjddh12345 <pk4win7@naver.com>
2023-10-05 13:46:19 +0900

    작성된 게시물을 리스트에 저장, 리스트 기능도 구현

--- src/com/pjo/java/ssg/Main.java
+++ src/com/pjo/java/ssg/Main.java
@@ -1,5 +1,7 @@
 package com.pjo.java.ssg;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,43 +9,67 @@ public class Main {
 		System.out.println("== 프로그램 시작 == ");
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		int lastArticleId = 0;
 		
-		while ( true ) {
+		List<Article> articles = new ArrayList();
+
+		while (true) {
 			System.out.printf("명령어) ");
 			String command = sc.nextLine();
-			
+
 			command = command.trim();
-			
-			if ( command.length() == 0 ) {
+
+			if (command.length() == 0) {
 				continue;
 			}
-			
-			if( command.equals("system exit")) {
+
+			if (command.equals("system exit")) {
 				break;
-			}
-			else if ( command.equals("article write")) {
+			} else if (command.equals("article write")) {
 				int id = lastArticleId + 1;
 				lastArticleId = id;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
 				
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
-			}
-			else if ( command.equals("article list")) {
-				System.out.println("게시물이 없습니다.");
-			}
-			else {
+			} else if (command.equals("article list")) {
+				if ( articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+				
+				System.out.println("번호, 제목");
+				for ( int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					
+					System.out.printf("%d, %s\n", article.id, article.title);
+				}
+				
+			} else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
 			}
 		}
-		
+
 		sc.close();
 
-
 		System.out.println("== 프로그램 끝 == ");
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
